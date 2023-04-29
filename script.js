5a828a4715af812b1d9beb83d1a4b4a436ec4360
SinaSh10 <sinash10.job@gmail.com>
2023-04-29 16:46:49 +0430

    victory rule

--- script.js
+++ script.js
@@ -1,8 +1,34 @@
-let currentOne=document.querySelector('#currentOne')
-let totalOne=document.querySelector('#totalOne')
+let playerName0=prompt('first player name')
+let playerName1=prompt('second player name')
+document.querySelector('#playerName0').innerHTML=playerName0
+document.querySelector('#playerName1').innerHTML=playerName1
+
+let current0=document.querySelector('#current0')
+let total0=document.querySelector('#total0')
+let current1=document.querySelector('#current1')
+let total1=document.querySelector('#total1')
 let images=document.querySelectorAll('img')
-let score=0
-let total=0
+let activePlayer=0
+let score=[0,0]
+let total=[0,0]
+function change(){
+    document.querySelector('.player-0').classList.toggle('currentPlayer')
+    document.querySelector('.player-1').classList.toggle('currentPlayer')
+    activePlayer===0? activePlayer=1:activePlayer=0
+}
+function victory(){
+    if(total[activePlayer]>=100){
+        alert(`${activePlayer} win the game`)
+        total=[0,0]
+    score=[0,0]
+    total0.innerHTML=0
+    total1.innerHTML=0
+    score0.innerHTML=0
+    score1.innerHTML=0
+    images[ranNumber-1].classList.add('hidden')
+    }
+    
+}
 document.querySelector('#dice').addEventListener('click',()=>{
     let ranNumber=Math.ceil(Math.random()*6)
     for(let i=0;i<=5;i++){
@@ -11,22 +37,34 @@ document.querySelector('#dice').addEventListener('click',()=>{
         }
         else{ images[i].classList.add('hidden')}
     }
-   if(ranNumber!==1){
-    score+=ranNumber
-   }
-   else{
-    score=0
+    
+        if(ranNumber!==1){
+            score[activePlayer]+=ranNumber
+            document.querySelector(`#current${activePlayer}`).innerHTML=score[activePlayer]
+           }
+           else{
+            score[activePlayer]=0
+            document.querySelector(`#current${activePlayer}`).innerHTML=score[activePlayer]
+            change()
+        }
+    
+    
   
-}
-   currentOne.innerHTML=score
+  
   
 })
 document.querySelector('#save').addEventListener('click',()=>{
-   total+=score
-   totalOne.innerHTML=total
-   score=0
-   currentOne.innerHTML=score
-   for(let j=0;j<=5;j++){
-    images[j].classList.add('hidden')
-   } 
+    total[activePlayer]+=score[activePlayer]
+    document.querySelector(`#total${activePlayer}`).innerHTML=total[activePlayer]
+    score[activePlayer]=0
+    document.querySelector(`#current${activePlayer
+    }`).innerHTML=score[activePlayer]
+   
+    victory()
+    change() 
+   
+   
+   
+   
 })   
+
